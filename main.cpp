5098c0f5d8886258958f7a303ec56f3ed921eb84
a-einstein1879 <a.einstein1879@yandex.ru>
2016-03-03 17:57:34 +0300

    Removed useless code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include "market.h"
 #include <stdlib.h>
 
-#include "object.h"
-#include "linklist.h"
-
 int main() {
 	Market *market;
 	market = market->getMarket();
@@ -11,18 +8,6 @@ int main() {
 	for(int i = 0; i < 8; i++) {
 		market->tick();
 	}
-/*	Object obj1(1, 2, 1);
-	Object obj2(2, 2, 1);
-	LinkList list1;
-	list1.push(obj1);
-	list1.push(obj2);
-	list1.view();
-
-	list1.pop(1);
-	list1.view();
-
-	list1.push(obj2, 1);
-	list1.view();*/
 
 	system("pause");
 	return 0;
