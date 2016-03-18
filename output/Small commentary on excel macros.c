487b5e9fed4899cae21a0d78451268003827dfe7
a-einstein1879 <a.einstein1879@yandex.ru>
2016-03-18 16:16:58 +0300

    Commentary on macros

---
+++ output/Small commentary on excel macros.c
@@ -0,0 +1,3 @@
+Due to some reasons. personal.xlsb that can be modified is stored at C:\Users\Ian\AppData\Roaming\Microsoft\Excel\XLSTART
+But the personal.xlsb that is called from excel documents is stored at C:\Users\Ian\Documents
+And we have to copy first one to the place of another one every time we change it
